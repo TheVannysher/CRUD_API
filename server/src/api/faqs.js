2d5d12d2d44319c8027fe3d975366b38d789bb1c
Albert <albert@pop-os.localdomain>
2020-08-07 11:01:52 -0400

    initial commit

---
+++ server/src/api/faqs.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const monk = require('monk');
+
+const db = monk(process.env.MONGO_URI);
+const faqs = db.get('faqs')
+
+const router = express.Router();
+
+// read all
+router.get('/', async (req,res,next) => {
+    try {
+        const items = await faqs.find({});
+        res.json(items)
+    } catch (error) {
+        next(error);
+    }
+});
+// read one
+router.get('/:id', (req,res,next) => {
+    res.json({
+        message: 'Read One',
+    });
+});
+// create one
+router.post('/', (req,res,next) => {
+    res.json({
+        message: 'Create One',
+    });
+});
+// update one
+router.put('/:id', (req,res,next) => {
+    res.json({
+        message: 'Update One',
+    });
+});
+// delete one
+router.delete('/:id', (req,res,next) => {
+    res.json({
+        message: 'Delete One',
+    });
+});
+
+module.exports = router;
