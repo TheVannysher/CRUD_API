bf73d9d8bb04e5a2dd28c7aa2bea8265aa626454
TheVannysher <thevannysher@pop-os.localdomain>
2020-08-08 23:49:11 -0400

    almost done!

--- server/src/api/faqs.js
+++ server/src/api/faqs.js
@@ -1,8 +1,27 @@
+/**
+ *  --- Notes ---
+ *  
+ *  There is no valdations done in this example!
+ * 
+ * 
+ * 
+ * 
+ */
 const express = require('express');
 const monk = require('monk');
+const Joi = require('@hapi/joi');
 
 const db = monk(process.env.MONGO_URI);
 const faqs = db.get('faqs');
+
+
+//change schema for your needs
+// for faqs : 
+const schema = Joi.object({
+    question : Joi.string().trim().required(),
+    answer : Joi.string().trim().required(),
+    video_url : Joi.string().uri()
+});
 
 const router = express.Router();
 
@@ -14,30 +33,61 @@ router.get('/', async (req,res,next) => {
     } catch (error) {
         next(error);
     }
+    
 });
 // read one
-router.get('/:id', (req,res,next) => {
-    res.json({
-        message: 'Read One',
-    });
+router.get('/:id', async (req,res,next) => {
+    try {
+        const {id} = req.params;
+        const item = await faqs.findOne({
+            _id : id,
+        });
+        if(!item) return next();
+        return res.json(item);
+    } catch (error) {
+        next(error);
+    }
 });
 // create one
-router.post('/', (req,res,next) => {
-    res.json({
-        message: 'Create One',
-    });
+router.post('/', async (req,res,next) => {
+    try {
+        const value = await schema.validateAsync(req.body);
+        const inserted = await faqs.insert(value);
+        res.json(inserted);
+    } catch (error) {
+        next(error);
+    }
 });
 // update one
-router.put('/:id', (req,res,next) => {
-    res.json({
-        message: 'Update One',
-    });
+router.put('/:id', async (req,res,next) => {
+    try {
+        const {id} = req.params;
+        const value = await schema.validateAsync(req.body);
+        const item = await faqs.findOne({
+            _id : id,
+        });
+        if(!item) return next();
+        await faqs.update({
+            _id : id,
+        }, {
+            $set: value
+        });
+        res.json(value);
+    } catch (error) {
+        next(error);
+    }
 });
 // delete one
-router.delete('/:id', (req,res,next) => {
-    res.json({
-        message: 'Delete One',
-    });
+router.delete('/:id', async (req,res,next) => {
+    try {
+        const { id } = req.params;
+        await faqs.delete({_id : id});
+        res.json({
+            message: "Successfully Deleted"
+        })
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
