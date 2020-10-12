ffd0f4ef57b6c4b3f716b0ffb26a96af446639fc
Albert <armadwarf@hotmail.com>
2020-10-12 14:09:57 -0400

    routes almost finished

--- server/src/api/faqs.js
+++ server/src/api/faqs.js
@@ -19,7 +19,7 @@ const schema = joi.object(
         question : joi.string().trim().required(),
 
         answer : joi.string().trim().required(),
-        
+
         time : joi.date()
         
     }
@@ -36,28 +36,73 @@ router.get('/',async (req,res,next) => {
 });
 
 //read one
-router.get('/:id',(req,res,next) => {
-    res.json({
-        message : 'read one'
-    });
+router.get('/:id', async (req,res,next) => {
+    try {
+        const {id} = req.params;
+        const item = await faqs.findOne({
+            _id: id
+        })
+        if(!item){
+            next();
+        }
+        return res.json(item);
+    } catch (error) {
+        next(error)
+    }
 })
 //create one
-router.post('/',(req,res,next) => {
-    res.json({
-        message : 'create one'
-    });
+router.post('/', async (req,res,next) => {
+    try {
+        console.log(req.body)
+        const value = await schema.validateAsync(req.body)
+        // res.json(value)
+        //TODO : validations 
+        const inserted = await faqs.insert(value);
+        res.json(inserted)
+    } catch (error) {
+        next(error)
+    }
 })
 //update one
-router.put('/:id',(req,res,next) => {
-    res.json({
-        message : 'update one'
-    });
+router.put('/:id',async(req,res,next) => {
+    try {
+        const {id} = req.params;
+        const value = await schema.validateAsync(req.body)
+        const item = await faqs.findOne({
+            _id: id
+        })
+        if(!item){
+            next();
+        }
+        // res.json(value)
+        //TODO : validations 
+        await faqs.update({
+            _id : id
+        },{
+            $set : value,
+        });
+        res.json(value)
+    } catch (error) {
+        next(error)
+    }
 })
 //delete one
-router.delete('/:id',(req,res,next) => {
-    res.json({
-        message : 'delete one'
-    });
+router.delete('/:id', async (req,res,next) => {
+    try {
+        const {id} = req.params;
+        const item = await faqs.findOne({
+            _id: id
+        })
+        if(!item){
+            next();
+        }
+        await faqs.remove({_id : id})
+        res.json({
+            message : "success"
+        })
+    } catch (error) {
+        next(error)
+    }
 })
 
 
